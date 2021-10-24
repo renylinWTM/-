309091da92f03db74f9fa54d6f014385137941df
renylinWTM <85269871+renylinWTM@users.noreply.github.com>
2021-10-24 15:51:05 +0800

    Add files via upload

---
+++ Vito'sfamily.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int testcast;
+    cin >> testcast;
+    while (testcast--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        int mid = n / 2;
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum = sum + abs(a[i] - a[mid]);
+        }
+        cout << sum << endl;
+    }
+}
