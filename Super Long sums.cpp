75270f5bb809e7ecd6e34d41eba4ca69c399c420
renylinWTM <85269871+renylinWTM@users.noreply.github.com>
2021-11-08 11:49:25 +0800

    Update Super Long sums.cpp

--- Super Long sums.cpp
+++ Super Long sums.cpp
@@ -1,27 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  int T, M;
-  char a, b;
-  cin >> T;
-  while (T--){
-    cin >> M;
-    string s1 = "", s2 = "";
-    for (int i=0; i<M; i++){
-      cin >> a >> b;
-      s1 += a;
-      s2 += b;
-    }
-    s2 += '0';
-    for (int i=M-1; i>=0; i--){
-      int n1 = s1[i] - '0';
-      int n2 = s2[i] - '0';
-      int carry = s2[i+1] - '0';
-      int tmp = n1 + n2 + carry;
-      s1[i] = '0' + (tmp)%10;
-      s2[i] = '0' + (tmp)/10;
-    }
-    cout << s1 << "\n\n";
-  }
-}
+	int n,m;
+	char a,b;
+	cin>>n;
+	int count=0;
+	while(n--){
+		cin>>m;
+		if(count==1){
+			cout<<endl;
+		}
+		string str1="",str2="";
+		for(int i=0;i<m;i++){
+			cin>>a>>b;
+			str1=str1+a;
+			str2=str2+b;
+		} 
+		str2=str2+'0';
+		for(int i=m-1;i>=0;i--){
+			int n1=str1[i]-'0';
+			int n2=str2[i]-'0';
+			int carry=str2[i+1]-'0';
+			int tmp = n1 + n2 + carry;
+     	 	str1[i] = '0' + (tmp)%10;
+      		str2[i] = '0' + (tmp)/10;
+		}
+		cout<<str1<<endl;
+		count=1;
+	}
 }
